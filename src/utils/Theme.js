1c3de41d1a4d88548db78f674deb4fc0e18746dc
ozturkselim99 <ozturkselim99@gmail.com>
2021-11-06 17:36:58 +0300

    uygulamanın renk kodları belirlendi.

---
+++ src/utils/Theme.js
@@ -0,0 +1,23 @@
+export const brandColors = {
+    dark: '#001A83',
+    default: '#002DE3',
+    darkMode: '#375FFF',
+    light: '#879FFF',
+    background: '#D2D5F9',
+};
+export const neutralColors = {
+    active: '#0F1828',
+    dark: '#152033',
+    body: '#1B2B48',
+    weak: '#A4A4A4',
+    disabled: '#ADB5BD',
+    line: '#EDEDED',
+    offWhite: '#F7F7FC',
+    white: '#FFFFFF',
+};
+export const accentColors={
+    danger: '#E94242',
+    warning: '#FDCF41',
+    success: '#2CC069',
+    safe: '#7BCBCF',
+};
