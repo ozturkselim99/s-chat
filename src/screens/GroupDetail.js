ee5737a5488b8d4e764e45f46b4772ebe788f421
ozturkselim99 <ozturkselim99@gmail.com>
2021-12-15 21:55:08 +0300

    grubu olusturan kisi tarafından silme islemi saglandi

--- src/screens/GroupDetail.js
+++ src/screens/GroupDetail.js
@@ -1,15 +1,16 @@
-import React, {useLayoutEffect, useState} from "react";
-import {SafeAreaView, View, Image, Text, FlatList, Switch} from "react-native";
-import {brandColors, neutralColors} from "../utils/Theme";
+import React, {useEffect, useLayoutEffect, useState} from "react";
+import {Alert, FlatList, Image, LogBox, SafeAreaView, Switch, Text, TouchableOpacity, View} from "react-native";
+import {accentColors, brandColors, neutralColors} from "../utils/Theme";
 import {StatusBar} from "expo-status-bar";
 import CustomMemberItem from "../components/CustomMemberItem";
+import Icon from "react-native-vector-icons/Ionicons";
+import {auth, db} from "../../firebase";
 
 const GroupDetail = ({navigation, route}) => {
 
-    const {members, groupName} = route.params;
+    const {members, groupName, creator, createdAt, creatorUserId, groupId} = route.params;
     const [isEnabled, setIsEnabled] = useState(false);
     const toggleSwitch = () => setIsEnabled(previousState => !previousState);
-
     useLayoutEffect(() => {
         navigation.setOptions({
             title: "",
@@ -19,9 +20,43 @@ const GroupDetail = ({navigation, route}) => {
             headerTintColor: "white"
         });
     }, [])
+    useEffect(()=>{
+        LogBox.ignoreLogs(['Setting a timer']);
+    })
+
+    const deleteGroup = () => {
+        Alert.alert(
+            "Grubu sil",
+            "Grubu silmek istediğinizden emin misiniz ?",
+            [
+                {
+                    text: "Hayır",
+                    onPress: () => console.log("Cancel Pressed"),
+                    style: "cancel"
+                },
+                {
+                    text: "Evet", onPress: async () => {
+                        try {
+                            await db.collection("groups")
+                                .doc(groupId)
+                                .delete()
+                                .then(() => {
+                                    navigation.navigate("Groups")
+                                });
+                        } catch (err) {
+                            console.log(err)
+                        }
+
+                    }
+                }
+            ]
+        )
+
+    }
 
     const renderItem = ({item}) => (
-        <CustomMemberItem name={item.member.name} status={item.member.status} imageURL={item.member.imageURL}/>
+        <CustomMemberItem id={item.member.userId} name={item.member.name} status={item.member.status}
+                          imageURL={item.member.imageURL}/>
     );
 
     const itemSeparator = () => (
@@ -41,14 +76,40 @@ const GroupDetail = ({navigation, route}) => {
                     }}
                 />
                 <View style={{flexDirection: "column", flex: 1, marginLeft: 16}}>
-                    <Text style={{
-                        color: neutralColors.white,
-                        fontSize: 18,
-                    }}>{groupName}</Text>
+                    <View style={{
+                        alignItems: "center",
+                        flexDirection: "row"
+                    }}>
+                        <Text style={{
+                            color: neutralColors.white,
+                            fontSize: 18,
+                        }}>{groupName}</Text>
+                        {creatorUserId == auth.currentUser.uid && (
+                            <TouchableOpacity onPress={deleteGroup} style={{
+                                borderRadius: 50,
+                                backgroundColor: accentColors.danger,
+                                width: 25,
+                                height: 25,
+                                alignItems: "center",
+                                justifyContent: "center",
+                                marginLeft: "auto"
+                            }}>
+                                <Icon
+                                    name="trash"
+                                    color={neutralColors.white}
+                                    size={16}
+                                />
+                            </TouchableOpacity>)}
+                    </View>
+
                     <Text style={{
                         fontSize: 12,
                         color: neutralColors.disabled
                     }}>{members.length} members</Text>
+                    <Text style={{
+                        fontSize: 12,
+                        color: neutralColors.disabled
+                    }}>Created by {creator} on {createdAt.toDate().toDateString()}</Text>
                 </View>
             </View>
             <View style={{paddingHorizontal: 24, paddingTop: 8, flex: 1}}>
@@ -69,8 +130,8 @@ const GroupDetail = ({navigation, route}) => {
                     </View>
                     <Switch
                         style={{marginLeft: "auto"}}
-                        trackColor={{false: "#767577", true: "#81b0ff"}}
-                        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+                        trackColor={{false: "#767577", true: brandColors.darkMode}}
+                        thumbColor={isEnabled ? brandColors.dark : "#f4f3f4"}
                         ios_backgroundColor="#3e3e3e"
                         onValueChange={toggleSwitch}
                         value={isEnabled}
@@ -87,6 +148,7 @@ const GroupDetail = ({navigation, route}) => {
                     ItemSeparatorComponent={itemSeparator}
                     data={members}
                     renderItem={renderItem}
+                    keyExtractor={(item, index) => index}
                 >
                 </FlatList>
             </View>
